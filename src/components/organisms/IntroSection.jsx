105f07ceaa3c04e15bbaf86b71aeee6759d16ad1
LeeSyong <jejelsk27@naver.com>
2022-09-30 06:10:18 +0900

    Feat: organisms/IntroSection 컴포넌트 생성 및 스토리 추가

---
+++ src/components/organisms/IntroSection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import Intro from "../molecules/Intro";
+
+function IntroSection({ onClick }) {
+  return (
+    <IntroSectionStyled>
+      <Intro onClick={onClick} />
+    </IntroSectionStyled>
+  );
+}
+
+export default IntroSection;
+
+const IntroSectionStyled = styled.section`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+IntroSection.proptypes = {
+  onClick: PropTypes.func,
+};

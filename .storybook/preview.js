87dbc6cae40e26d5efd10e20af0913bede2de70f
LeeSyong <jejelsk27@naver.com>
2022-09-29 22:09:08 +0900

    Add: GlobalStyle 추가 및 storybook 관련 초기 설정 완료
    - GlobalStyle 추가 및 적용
    - ReactDOM.render 대신 createRoot 사용
    - storybook 설치 시 종속성 에러 해결 위해 .npmrc 파일 추가
    - storybook에 GlobalStyle 설정 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,15 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 
+import GlobalStyle from "../src/styles/GlobalStyle";
+
 export const decorators = [
   withRouter,
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
 ];
 
 export const parameters = {
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import GlobalStyle from "./styles/GlobalStyle";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>App</div>
+    </>
+  );
 }
 
 export default App;

3f3950c2f29b76ef995b2e99049fabf1a2603b57
LeeSyong <jejelsk27@naver.com>
2022-10-01 01:21:36 +0900

    Fix: QuizSection.stories.js 파일 수정

--- src/components/organisms/QuizSection.stories.js
+++ src/components/organisms/QuizSection.stories.js
@@ -10,7 +10,7 @@ const Template = (args) => <QuizSection {...args} />;
 export const TestQuizSection = Template.bind({});
 TestQuizSection.args = {
   quizData: {
-    question: "Q. 이것은 무엇입니까?",
+    question: "이것은 무엇입니까?",
     answers: ["보기 1", "보기 2", "보기 3", "보기 4"],
   },
   selectedAnswer: "보기 2",

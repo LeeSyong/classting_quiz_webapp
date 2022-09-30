19e682af203240d78c62e3c2174c5ef4bc1217be
LeeSyong <jejelsk27@naver.com>
2022-10-01 05:01:56 +0900

    Add: ResultSection.stories.js 파일 수정

--- src/components/organisms/ResultSection.stories.js
+++ src/components/organisms/ResultSection.stories.js
@@ -12,6 +12,7 @@ const Template = (args) => <ResultSection {...args} />;
 export const TestResultSection = Template.bind({});
 TestResultSection.args = {
   chartData: [3, 2],
+  elapsedTime: "0분 8초",
   handleRetryButtonClick: () => navigate("/quiz/1"),
   handleNoteButtonClick: () => navigate("/notes"),
 };

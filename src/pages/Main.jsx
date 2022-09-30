df2deba02c0588133aff222de783e05cdf450a33
LeeSyong <jejelsk27@naver.com>
2022-10-01 00:04:23 +0900

    Feat: Main 페이지 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchQuiz } from "../store/quizSlice";
+
+import IntroSection from "../components/organisms/IntroSection";
+import BaseTemplate from "../components/templates/BaseTemplate";
 
 function Main() {
-  return <div>Main</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleStartClick = () => {
+    dispatch(fetchQuiz());
+
+    navigate("/quiz/1");
+  };
+
+  return (
+    <BaseTemplate children={<IntroSection onClick={handleStartClick} />} />
+  );
 }
 
 export default Main;

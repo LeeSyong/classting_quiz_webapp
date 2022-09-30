affd5b5f2d768a936a3fbfdfe7f1913834eef45d
LeeSyong <jejelsk27@naver.com>
2022-10-01 01:16:13 +0900

    Feat: molecules/Examples 컴포넌트 수정 및 palette 색상 추가

--- src/components/molecules/Examples.stories.js
+++ src/components/molecules/Examples.stories.js
@@ -9,22 +9,5 @@ const Template = (args) => <Examples {...args} />;
 
 export const TestExamples = Template.bind({});
 TestExamples.args = {
-  examples: [
-    {
-      id: 0,
-      content: "보기 1",
-    },
-    {
-      id: 1,
-      content: "보기 2",
-    },
-    {
-      id: 2,
-      content: "보기 3",
-    },
-    {
-      id: 3,
-      content: "보기 4",
-    },
-  ]
+  examples: ["보기 1", "보기 2", "보기 3", "보기 4"],
 };

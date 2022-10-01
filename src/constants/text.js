6d4a04945a634b24690ce32ff8127441a9dfa5a0
LeeSyong <jejelsk27@naver.com>
2022-10-01 16:38:03 +0900

    Feat: Notes 페이지 구현 및 organisms/NotesSection 컴포넌트 생성
    - Notes 페이지 구현
    - organisms/NotesSection 컴포넌트 생성
    - constants/text 파일 수정

--- src/constants/text.js
+++ src/constants/text.js
@@ -5,10 +5,21 @@ const INTRO = [
   "각 문제에 대한 오답 노트를 작성하실 수 있습니다.",
 ];
 
-const ERROR = "에러가 발생했습니다. 다시 시도해주세요.";
+const ERROR = {
+  CANNOT_GET_QUIZ: "퀴즈를 가져올 수 없습니다. 다시 시도해주세요.",
+  CANNOT_GET_RESULT:
+    "결과를 가져오는 데 에러가 발생했습니다. 다시 시도해주세요.",
+  CANNOT_GET_NOTE: "오답 노트에 접근할 수 없습니다. 다시 시도해주세요.",
+  CANNOT_WRITE_NOTE: "에러로 인해 오답 노트를 작성할 수 없습니다.",
+};
+
+const INFO = {
+  SOLVE_THE_QUIZZES: "먼저 '퀴즈 풀기' 버튼을 눌러 퀴즈를 풀어주세요.",
+  CONFIRM_TO_SAVE: "빈 값을 저장하시겠습니까? 이전 내용이 사라집니다.",
+};
 
 const REVIEW_MENU = ["다시 풀기", "오답 노트"];
 
 const RESULT_TABLE_HEAD = ["풀이 시간", "정답 개수", "오답 개수"];
 
-export { INTRO, ERROR, REVIEW_MENU, RESULT_TABLE_HEAD };
+export { INTRO, ERROR, INFO, REVIEW_MENU, RESULT_TABLE_HEAD };
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { decode } from "html-entities";
+
+import BaseTemplate from "../components/templates/BaseTemplate";
+import NoteSection from "../components/organisms/NotesSection";
+
+import { ERROR, INFO } from "../constants/text";
 
 function Notes() {
-  return <div>Notes</div>;
+  const navigate = useNavigate();
+  const { status, data, error } = useSelector((state) => state.quizzes);
+  const [quizResults, setQuizResults] = useState([]);
+
+  const handleWriteButtonClick = useCallback(
+    (_, id) => navigate(`/notes/${id}`),
+    [navigate],
+  );
+
+  useEffect(() => {
+    if (status === "idle" && data.allIds.length) {
+      const quizResults = data.allIds.map((id) => {
+        const quizResult = data.byId[id];
+
+        return {
+          ...quizResult,
+          question: decode(quizResult.question),
+          correct_answer: decode(quizResult.correct_answer),
+          selected_answer: decode(quizResult.selected_answer),
+        };
+      });
+
+      setQuizResults(quizResults);
+
+      return;
+    }
+
+    if (status === "idle" && !data.allIds.length) {
+      alert(INFO.SOLVE_THE_QUIZZES);
+    }
+
+    if (status === "idle" && error) {
+      alert(ERROR.CANNOT_GET_NOTE);
+    }
+
+    navigate("/");
+  }, [status, data, error, navigate]);
+
+  return (
+    <BaseTemplate
+      children={
+        <NoteSection
+          quizResults={quizResults}
+          onClick={handleWriteButtonClick}
+        />
+      }
+    />
+  );
 }
 
 export default Notes;
